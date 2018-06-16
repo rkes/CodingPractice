73e477c994c44472fd81769780758efc13c6f7cb
rkes <rakesh.nit.mca@gmail.com>
2018-06-17 01:51:06 +0530

    Tree

--- src/main/java/com/general/BuildTreeFromInOrderPost.java
+++ src/main/java/com/general/BuildTreeFromInOrderPost.java
@@ -7,20 +7,30 @@ public class BuildTreeFromInOrderPost {
 		int []postOderar={1} ;
 		index = 0;;
 	    
-        int in[] = new int[]{9, 8, 4, 2, 10, 5, 10, 1, 6, 3, 13, 12, 7};
-        int pre[] = new int[]{1, 2, 4, 8, 9, 5, 10, 10, 3, 6, 7, 12, 13};
+        int in[] = new int[]{ 1, 2, 3};
+        int pre[] = new int[]{2, 1, 3  };
         int len = in.length;
 		TreeNode node = buildInOrderPre(in, pre, 0, len-1);//build(inOderar,postOderar,0,inOderar.length-1);
-		printInOrder(node);
+		printPreOrder(node);
+		System.out.println();
+		printPostOrder(node);
 	}
-	public static void printInOrder(TreeNode node){
+	public static void printPreOrder(TreeNode node){
 		if(node != null){
-			printInOrder(node.left);
 			System.out.printf("%d   ",node.val);
-			printInOrder(node.right);
+			printPreOrder(node.left);
+			printPreOrder(node.right);
 		}
 	}
-	public static TreeNode build(int []inOrderAr,int []postOrderAr,int start,int end){
+	public static void printPostOrder(TreeNode node){
+		if(node != null){
+		
+			printPostOrder(node.left);
+			printPostOrder(node.right);
+			System.out.printf("%d   ",node.val);
+		}
+	}
+/*	public static TreeNode build(int []inOrderAr,int []postOrderAr,int start,int end){
 		if(start > end) return null;
 		int elem = postOrderAr[index--];
 		int idx = search(inOrderAr,start,end,elem);	
@@ -28,14 +38,13 @@ public class BuildTreeFromInOrderPost {
 		trNode.right = build(inOrderAr, postOrderAr, idx+1,end);
 		trNode.left = build(inOrderAr, postOrderAr, start, idx-1);
 		return trNode;
-	}
+	}*/
 	public static TreeNode buildInOrderPre(int []inOrderAr,int []preOrderAr,int start,int end){
 		if(start > end) return null;
-		int elem = preOrderAr[index++];
-		int idx = search(inOrderAr,start,end,elem);	
-		TreeNode trNode = new TreeNode(elem);
+		TreeNode trNode = new TreeNode(preOrderAr[index++]);
 		if(start == end)
 			return trNode;
+		int idx = search(inOrderAr,start,end,trNode.val);	
 		trNode.left = buildInOrderPre(inOrderAr, preOrderAr, start,idx-1);
 		trNode.right = buildInOrderPre(inOrderAr, preOrderAr, idx+1,end);
 		return trNode;
